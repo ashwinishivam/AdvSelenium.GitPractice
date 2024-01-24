ee78dc1f4dd10e03b67ea2617ef71ab2bab94678
djs <djs@DESKTOP-5MTUOF7>
2024-01-24 15:57:12 +0530

    Commited

---
+++ src/main/java/GitHubPractice.java
@@ -0,0 +1,14 @@
+
+public class GitHubPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("GoodMorning");
+		System.out.println("C="+c);
+	}
+
+}
